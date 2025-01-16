96b9b1ab731fa66c78626b17f32a2923a6d9e4ce
Jason Lothamer <jhlothamer@gmail.com>
2025-01-16 16:15:16 -0500

    add play button and copy path to open editors item

--- addons/open_editors/open_editors_item.gd
+++ addons/open_editors/open_editors_item.gd
@@ -7,6 +7,7 @@ extends PanelContainer
 @onready var _close_btn:TextureButton = %CloseBtn
 @onready var _label:Label = %TextLabel
 @onready var _icon:TextureRect = %IconTextureRect
+@onready var _play_btn:TextureButton = %PlaySceneBtn
 
 
 var scene_path := ""
@@ -18,8 +19,9 @@ var icon:Texture2D
 func _ready() -> void:
 	_outline.visible = false
 	_label.text = display_name
-	tooltip_text = scene_path
+	tooltip_text = "%s\n(right click to copy path)" % scene_path
 	_close_btn.modulate = Color.TRANSPARENT
+	_play_btn.modulate = Color.TRANSPARENT
 	if icon: _icon.texture = icon
 
 
@@ -34,27 +36,26 @@ func _on_close_btn_pressed() -> void:
 
 
 func _on_gui_input(event: InputEvent) -> void:
-	if not event is InputEventMouseButton: return
+	if not event is InputEventMouseButton or not event.is_pressed(): return
 	var mb:InputEventMouseButton = event
 	if mb.button_index == MOUSE_BUTTON_LEFT:
 		EditorInterface.open_scene_from_path(scene_path)
+	elif mb.button_index == MOUSE_BUTTON_RIGHT:
+		EditorInterface.open_scene_from_path(scene_path)
+		DisplayServer.clipboard_set(scene_path)
+		print("Path for scene copied to clipboard: %s" % scene_path)
 
 
 func _on_mouse_entered() -> void:
 	_outline.visible = true
 	_close_btn.modulate = Color.WHITE
+	_play_btn.modulate = Color.WHITE
 
 
 func _on_mouse_exited() -> void:
 	_outline.visible = false
 	_close_btn.modulate = Color.TRANSPARENT
+	_play_btn.modulate = Color.TRANSPARENT
 
-
-func _on_close_btn_mouse_entered() -> void:
-	_outline.visible = true
-	_close_btn.modulate = Color.WHITE
-
-
-func _on_close_btn_mouse_exited() -> void:
-	_outline.visible = false
-	_close_btn.modulate = Color.TRANSPARENT
+func _on_play_scene_btn_pressed():
+	EditorInterface.play_custom_scene(scene_path)
